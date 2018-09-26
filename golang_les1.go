b989dee30430bf89e16087e64301f83030384e44
Misha Pacheskiy <mikki.s.fails@gmail.com>
2018-09-26 18:59:09 +0300

    lang les 1

---
+++ golang_les1.go
@@ -0,0 +1,42 @@
+/*
+Техно фичи ГО
+1. Мульти парадигменный : процедурная, функциональная, ОО.
+2. Многопоток. Concurrency
+3. Инструменты
+
+
+Типы данных:
+	Примитивные:
+		bool true/false
+		Числа:
+			целые:
+				int | int8, int16, int32, int64
+				uint | uint8=byte
+			Вещественные:
+				float32
+				float64
+			Комплексные:
+				complex64
+				complex128
+		Symbols:
+			rune (16 ричное )(uint16) ex: 'a'
+		Strings:
+			string ex: `a` or "a"
+				1) \a \b etc...
+				2)
+
+
+
+
+
+
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main1() {
+	fmt.Println("")
+}
