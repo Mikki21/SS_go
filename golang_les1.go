b13bb1783e8d789d7bad1dd911effce1550e0eaa
Misha Pacheskiy <mikki.s.fails@gmail.com>
2018-09-27 10:32:03 +0300

    New theory added

--- golang_les1.go
+++ golang_les1.go
@@ -24,9 +24,24 @@
 			string ex: `a` or "a"   ! `` doesnt read unicode symbols
 				1) \a \b etc...
 				2)\u \x \o
-
-
-
+Placeholders in GOLNAG
+	Uses with fmt.Printf("string",p), p is declared.
+		%v - prints the value.
+		%+v - if value is structured, shows name of lines in structure
+		%#v - prints a Go syntax representation of the value
+		%T - prints type of the value
+		%t -
+		%d - prints decimal value
+		%b - prints binary value
+		%c - prints char equivalent
+		%x - 16x value
+		%f - basic decimal formatting of float.
+		%e %E - форматирует флоат в научной нотации
+		%s - простой вывод строки
+		%q
+		%x (for string)
+		%p - прдставление ссылки.
+		%6.2f - ширина вывода и точность
 
 
 
@@ -39,6 +54,12 @@ import (
 	"fmt"
 )
 
-func main1() {
-	fmt.Println("")
+type point struct {
+	x, y int
+}
+
+func main() {
+
+	//p := point{1, 2}
+	fmt.Printf("%s\n", "\"string\"")
 }
